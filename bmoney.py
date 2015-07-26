42330b1a800f97576e11ed2ee81f905d05c4d2ff
Michael Flaxman <mflaxman@gmail.com>
2015-07-25 21:37:24 -0400

    stashing changes, needs lots of testing

--- bmoney.py
+++ bmoney.py
@@ -1,6 +1,4 @@
 import click
-#from clint.textui import click.echo, indent, prompt
-#from clint import arguments
 
 # bitmerchant
 from bitmerchant.wallet import Wallet
@@ -13,7 +11,9 @@ from blockcypher.utils import is_valid_address_for_coinsymbol, satoshis_to_btc
 from blockcypher.constants import COIN_SYMBOL_MAPPINGS
 
 from utils import (guess_network_from_mkey, guess_cs_from_mkey,
-        find_hexkeys_from_bip32masterkey, get_blockcypher_walletname_from_mpub)
+        find_hexkeypair_from_bip32key_bc, find_paths_from_bip32key_bc,
+        get_blockcypher_walletname_from_mpub, get_tx_url,
+        COIN_SYMBOL_TO_BMERCHANT_NETWORK, COIN_SYMBOL_LIST)
 
 
 # FIXME: use a public API key that can be stored in source code
@@ -26,7 +26,10 @@ assert BLOCKCYPHER_PUBLIC_API_KEY
 def display_balance_info(wallet_obj, verbose=False):
     mpub = wallet_obj.serialize_b58(private=False)
 
-    bc_wallet_name = get_blockcypher_walletname_from_mpub(mpub)
+    bc_wallet_name = get_blockcypher_walletname_from_mpub(
+            mpub=mpub,
+            subchain_indexes=[0, 1],
+            )
     if verbose:
         print('Wallet Name: %s' % bc_wallet_name)
 
@@ -45,16 +48,115 @@ def display_balance_info(wallet_obj, verbose=False):
     return
 
 
+def get_used_addresses(wallet_obj, verbose=False):
+    '''
+    Get addresses already used by the wallet
+    '''
+    mpub = wallet_obj.serialize_b58(private=False)
+
+    wallet_name = get_blockcypher_walletname_from_mpub(
+            mpub=mpub,
+            subchain_indexes=[0, 1],
+            )
+
+    wallet_details = get_wallet_details(
+            wallet_name=wallet_name,
+            api_key=BLOCKCYPHER_PUBLIC_API_KEY,
+            coin_symbol=guess_cs_from_mkey(mpub),  # FIXME: fails for BCY!
+            )
+
+    if verbose:
+        print(wallet_details)
+
+    # confirm this is correct
+    return set(wallet_details.get('addresses', []))
+
+
+def get_unused_addresses_on_subchain(wallet_obj, subchain_index,
+        num_addrs_to_return, used_addr_set=set()):
+    '''
+    Traverse a subchain_index for unused addresses
+
+    Returns a dict of the following form:
+        {'address': '1abc123...', 'path': 'm/0/9',}
+    '''
+    assert type(subchain_index) is int, subchain_index
+
+    subchain_wallet = wallet_obj.get_child(subchain_index)  # m/subchain_index
+
+    attempt_cnt = 0
+    addresses_found = []
+    while True:
+        addr = subchain_wallet.get_child(attempt_cnt).to_address()
+        if addr not in used_addr_set:
+            addresses_found.append({
+                'address': addr,
+                'path': 'm/%s/%s' % (subchain_index, attempt_cnt),
+                })
+
+        if len(addresses_found) >= num_addrs_to_return:
+            break
+
+        attempt_cnt += 1
+
+
+def get_unused_receiving_addresses(wallet_obj, num_addrs_to_return=5,
+        verbose=False):
+    used_addr_set = get_used_addresses(wallet_obj=wallet_obj, verbose=verbose)
+    return get_unused_addresses_on_subchain(
+            wallet_obj=wallet_obj,
+            subchain_index=0,  # external chain
+            num_addrs_to_return=num_addrs_to_return,
+            used_addr_set=used_addr_set,
+            )
+
+
+def get_unused_change_addresses(wallet_obj, num_addrs_to_return=1,
+        verbose=False):
+    used_addr_set = get_used_addresses(wallet_obj=wallet_obj, verbose=verbose)
+    return get_unused_addresses_on_subchain(
+            wallet_obj=wallet_obj,
+            subchain_index=1,  # internal chain
+            num_addrs_to_return=num_addrs_to_return,
+            used_addr_set=used_addr_set,
+            )
+
+
 def display_new_receiving_addresses(wallet_obj, verbose=False):
-    pass
+    mpub = wallet_obj.serialize_b58(private=False)
+
+    click.echo('-' * 75)
+    click.echo('Next 5 Unused %s Recieving Addresses (for people to send you funds):' %
+            COIN_SYMBOL_MAPPINGS[guess_cs_from_mkey(mpub)]['currency_abbrev']
+            )
+
+    unused_receiving_addresses = get_unused_receiving_addresses(
+            wallet_obj=wallet_obj,
+            num_addrs_to_return=5,
+            verbose=verbose,
+            )
+
+    for unused_receiving_address in unused_receiving_addresses:
+        click.echo('  %s (path is m/0/%s)' % (
+            unused_receiving_address['address'],
+            unused_receiving_address['path'],
+            ))
+    # TODO: add option for when there is no internet connection
+    # Just tells you to use the advanced settings to dump
+
+    return wallet_home_chooser(wallet_obj=wallet_obj, verbose=verbose,
+        show_instructions=True)
 
 
 def display_recent_txs(wallet_obj, verbose=False):
     mpub = wallet_obj.serialize_b58(private=False)
-    bc_wallet_name = get_blockcypher_walletname_from_mpub(mpub)
+    wallet_name = get_blockcypher_walletname_from_mpub(
+            mpub=mpub,
+            subchain_indexes=[0, 1],
+            )
 
     wallet_details = get_wallet_details(
-            wallet_name=bc_wallet_name,
+            wallet_name=wallet_name,
             api_key=BLOCKCYPHER_PUBLIC_API_KEY,
             coin_symbol=guess_cs_from_mkey(mpub),  # FIXME: fails for BCY!
             )
@@ -85,8 +187,12 @@ def send_funds(wallet_obj, verbose=False):
     if verbose:
         print(coin_symbol)
 
+    wallet_name = get_blockcypher_walletname_from_mpub(
+            mpub=mpub,
+            subchain_indexes=[0, 1],
+            )
     wallet_details = get_wallet_details(
-            wallet_name=get_blockcypher_walletname_from_mpub(mpub),
+            wallet_name=wallet_name,
             api_key=BLOCKCYPHER_PUBLIC_API_KEY,
             coin_symbol=coin_symbol,
             )
@@ -105,8 +211,12 @@ def send_funds(wallet_obj, verbose=False):
     # TODO: confirm value entered
     # TODO: add ability to set preference
 
+    wallet_name = get_blockcypher_walletname_from_mpub(
+            mpub=mpub,
+            wallet_name=wallet_name,
+            )
     inputs = [{
-            'wallet_name': get_blockcypher_walletname_from_mpub(mpub),
+            'wallet_name': wallet_name,
             'wallet_token': BLOCKCYPHER_PUBLIC_API_KEY,
             },
             ]
@@ -123,11 +233,18 @@ def send_funds(wallet_obj, verbose=False):
         print(json.dumps(outputs, indent=2))
         print('coin symbol: %s' % coin_symbol)
 
+    change_address = get_unused_change_addresses(
+            wallet_obj=wallet_obj,
+            num_addrs_to_return=1,
+            verbose=verbose,
+            )[0]['address']
+
     unsigned_tx = create_unsigned_tx(
         inputs=inputs,
         outputs=outputs,
+        change_address=change_address,
         coin_symbol=coin_symbol,
-        include_tosigntx=True,
+        verify_tosigntx=True,  # guarantees we are signing the right TX
         )
 
     if verbose:
@@ -136,10 +253,10 @@ def send_funds(wallet_obj, verbose=False):
 
     privkeyhex_list, pubkeyhex_list = [], []
     for input_address in get_input_addresses(unsigned_tx):
-        hexkey_dict = find_hexkeys_from_bip32masterkey(
+        hexkey_dict = find_hexkeypair_from_bip32key_bc(
             pub_address=input_address,
             master_key=mpriv,
-            #network=foo,  # FIXME: support all coins
+            network=guess_network_from_mkey(mpriv),  # FIXME: support all coins
             )
 
         err_msg = "Couldn't find %s traversing bip32 key" % input_address
@@ -176,8 +293,13 @@ def send_funds(wallet_obj, verbose=False):
         print('Broadcasted TX')
         print(json.dumps(broadcasted_tx, indent=2))
 
-    # TODO: add nice message with link to block explorer
     click.echo(broadcasted_tx['tx']['hash'])
+
+    tx_url = get_tx_url(
+            tx_hash=broadcasted_tx['tx']['hash'],
+            coin_symbol=coin_symbol,
+            )
+    click.echo(tx_url)
 
     # Display updated wallet balance info
     display_balance_info(wallet_obj=wallet_obj, verbose=verbose)
@@ -191,54 +313,155 @@ def generate_offline_tx(wallet_obj, verbose=False):
 
 
 def sweep_funds_from_privkey(wallet_obj, verbose=False):
-    pkey = click.prompt('What private key would you like to send from?',
-            type=str)
-    pkey_obj = PrivateKey.from_wif(pkey)
-    pkey_addr = pkey_obj.get_public_key().to_address()
-    pkey_hex = pkey_obj.get_key()  # used for raw signing
+    mpub = wallet_obj.serialize_b58(private=False)
+    coin_symbol = str(guess_cs_from_mkey(mpub))
 
-    # FIXME
-    click.echo('To Implement')  # note sweep value == -1
+    pkey = click.prompt('Enter a private key (in WIF format) to send from?',
+            type=str)
+    # TODO: error checking if invalid wif
+    pkey_obj = PrivateKey.from_wif(pkey)
+    pkey_addr = pkey_obj.get_public_key().to_address(compressed=True)
 
     if verbose:
-        click.echo('%s for %s' % (pkey_addr, pkey_hex))
+        click.echo('%s from %s' % (pkey_addr, pkey))
 
-    # TODO: sign and broadcast tx
+    # FIXME: sign and broadcast tx
 
-    return wallet_home_chooser(wallet_obj=wallet_obj, show_instructions=True,
-            verbose=verbose)
+    inputs = [{
+            'address': pkey_addr,
+            }, ]
+
+    dest_addr = get_unused_receiving_addresses(
+            wallet_obj=wallet_obj,
+            num_addrs_to_return=1,
+            verbose=verbose,
+            )[0]['address']
+
+    outputs = {
+            'address': dest_addr,
+            'value': -1,  # sweep value
+            }
+
+    unsigned_tx = create_unsigned_tx(
+        inputs=inputs,
+        outputs=outputs,
+        change_address=None,
+        coin_symbol=coin_symbol,
+        verify_tosigntx=True,  # guarantees we are signing the right TX
+        )
+
+    if verbose:
+        print('Unsigned TX:')
+        print(json.dumps(unsigned_tx, indent=2))
+
+    privkeyhex_list, pubkeyhex_list = [], []
+    for _ in unsigned_tx['tx']['inputs']:
+        privkeyhex_list.append(pkey_obj.get_key())
+        pubkeyhex_list.append(pkey_obj.get_public_key().get_key(
+            compressed=True))
+
+    if verbose:
+        print('Private Key List: %s' % privkeyhex_list)
+        print('Public Key List: %s' % pubkeyhex_list)
+
+    # sign locally
+    tx_signatures = make_tx_signatures(
+            txs_to_sign=unsigned_tx['tosign'],
+            privkey_list=privkeyhex_list,
+            pubkey_list=pubkeyhex_list,
+            )
+
+    if verbose:
+        print('TX Signatures: %s' % tx_signatures)
+
+    # FIXME: verify what is being signed is legit
+    # FIXME: add final confirmation before broadcast
+
+    broadcasted_tx = broadcast_signed_transaction(
+            unsigned_tx=unsigned_tx,
+            signatures=tx_signatures,
+            pubkeys=pubkeyhex_list,
+            coin_symbol=coin_symbol,
+    )
+
+    if verbose:
+        print('Broadcasted TX')
+        print(json.dumps(broadcasted_tx, indent=2))
+
+    click.echo(broadcasted_tx['tx']['hash'])
+    tx_url = get_tx_url(
+            tx_hash=broadcasted_tx['tx']['hash'],
+            coin_symbol=coin_symbol,
+            )
+    click.echo(tx_url)
+
+    # Display updated wallet balance info
+    display_balance_info(wallet_obj=wallet_obj, verbose=verbose)
+
+    return wallet_home_chooser(wallet_obj=wallet_obj, verbose=verbose,
+            show_instructions=True)
 
 
 def dump_private_keys(wallet_obj, verbose=False):
+    '''
+    Offline mechanism to dump everything
+    '''
 
-    pass
+    # TODO: pagination
+
+    click.echo('-'*50)
+    click.echo('  path,WIF,address')
+    for chain_int in (0, 2):
+        for current in range(0, 10):
+            path = "m/%d/%d" % (chain_int, current)
+            if current == 0:
+                if chain_int == 0:
+                    click.echo('External Chain Addresses:')
+                elif chain_int == 1:
+                    click.echo('Internal Chain Addresses:')
+            child_wallet = wallet_obj.get_child_for_path(path)
+            click.echo('  %s,%s,%s' % (
+                path,
+                child_wallet.export_to_wif(),
+                child_wallet.to_address(),
+                ))
+
+    return wallet_home_chooser(wallet_obj=wallet_obj, verbose=verbose,
+        show_instructions=True)
 
 
 def dump_active_addresses(wallet_obj, verbose=False):
+    mpub = wallet_obj.serialize_b58(private=False)
+
     click.echo('Displaying Public Addresses Only')
     click.echo('For Private Keys, please open bmoney with your Master Private Key:')
     click.echo('  $ bmoney --wallet=xpriv123...')
 
-    # BIP 32 Default Wallet
-    # https://github.com/bitcoin/bips/blob/master/bip-0032.mediawiki#The_default_wallet_layout
-    m0h_wallet = wallet_obj.get_child(0, is_prime=True)
+    used_addresses = list(get_used_addresses(wallet_obj=wallet_obj,
+            verbose=verbose))
+
+    # get active addresses
+    paths = find_paths_from_bip32key_bc(
+            pub_address_list=used_addresses,
+            master_key=mpub,
+            network=guess_network_from_mkey(mpub),
+            starting_pos=0,
+            depth=100,  # TODO: pagination
+            )
+
+    for cnt, used_addr in enumerate(used_addresses):
+        path_display = paths[cnt]
+        if not path_display:
+            path_display = 'deeper traversal needed'
+        click.echo('  %s (%s) - %s (%s)' % (
+                used_addr,
+                path_display,
+                # FIXME:
+                '0 satoshis',
+                '0 BTC',
+                ))
 
     # TODO: add boolean for whether or not they have a balance (if online)
-    # TODO: ability to paginate more
-
-    click.echo('-'*50)
-    click.echo('Internal Chain Addresses:')
-    for x in range(10):
-        curr_wallet = m0h_wallet.get_child(0).get_child(x)
-        path = 'm/0/0/%s' % x
-        click.echo('%s (%s)' % (curr_wallet.to_address(), path))
-
-    click.echo('-'*50)
-    click.echo('External Chain Addresses:')
-    for x in range(10):
-        curr_wallet = m0h_wallet.get_child(1).get_child(x)
-        path = 'm/0/1/%s' % x
-        click.echo('%s (%s)' % (curr_wallet.to_address(), path))
 
     return wallet_home_chooser(wallet_obj=wallet_obj, verbose=verbose,
         show_instructions=True)
@@ -248,6 +471,8 @@ def wallet_home_chooser(wallet_obj, verbose=False, show_instructions=True):
     '''
     Home menu selector for what to do
     '''
+    # TODO: change options based on whether or not we're online
+
     if show_instructions:
         click.echo('-'*75)
         click.echo('Here are your options:')
@@ -292,23 +517,30 @@ def wallet_home_chooser(wallet_obj, verbose=False, show_instructions=True):
 
 def wallet_home(wallet_obj, verbose=False, show_welcome_msg=True):
     '''
+    Loaded on bootup (and likely never again)
     '''
     mpub = wallet_obj.serialize_b58(private=False)
 
     if show_welcome_msg:
         if wallet_obj.private_key is None:
-            click.echo("You've opened your wallet in PUBLIC key mode, so you cannot sign transactions.")
+            click.echo("You've opened your wallet in PUBLIC key mode, so you CANNOT sign transactions.")
         else:
             click.echo("You've opened your wallet in PRIVATE key mode, so you CAN sign transactions.")
             click.echo("If you like, you can always open your wallet in PUBLIC key mode like this:")
             click.echo('  $ bmoney --wallet=%s' % mpub)
 
+    wallet_name = get_blockcypher_walletname_from_mpub(
+            mpub=mpub,
+            subchain_indexes=[0, 1],
+            )
+
     # Instruct blockcypher to track the wallet by pubkey
     create_hd_wallet(
-            wallet_name=get_blockcypher_walletname_from_mpub(mpub),
+            wallet_name=wallet_name,
             xpubkey=mpub,
             api_key=BLOCKCYPHER_PUBLIC_API_KEY,
             coin_symbol=guess_cs_from_mkey(mpub),
+            subchain_indexes=[0, 1],  # for internal and change addresses
             )
 
     # Display balance info
@@ -317,6 +549,23 @@ def wallet_home(wallet_obj, verbose=False, show_welcome_msg=True):
     # Go to home screen
     return wallet_home_chooser(wallet_obj=wallet_obj, verbose=verbose,
             show_instructions=True)
+
+
+def coin_symbol_chooser(verbose=False):
+    click.echo('bwallet currently supports:')
+    for coin_symbol_choice in COIN_SYMBOL_LIST:
+        click.echo('  %s: %s' % (
+            coin_symbol_choice,
+            COIN_SYMBOL_MAPPINGS[coin_symbol_choice]['display_name'],
+            ))
+    coin_symbol = click.prompt('Which currency do you want to create a wallet for?').lower()
+    if verbose:
+        click.echo(coin_symbol)
+    if coin_symbol in COIN_SYMBOL_LIST:
+        return coin_symbol
+    else:
+        click.echo('`%s` is not a valid entry' % coin_symbol)
+        return coin_symbol_chooser()
 
 
 @click.command()
@@ -358,11 +607,20 @@ def cli(wallet, verbose):
 
     else:
         click.echo("You've opened your wallet without specifying a master public or master private key.")
-        click.echo("Let's generate a new master private key (locally) for you to use")
+        click.echo("Let's generate a new master private key (locally) for you to use.")
 
-        # TODO: add extra_entropy protection for compromised CSPRNG
-        # TODO: support other coins
-        user_wallet_obj = Wallet.new_random_wallet()
+        coin_symbol = coin_symbol_chooser(verbose=verbose)
+        network = COIN_SYMBOL_TO_BMERCHANT_NETWORK[coin_symbol]
+
+        click.echo("Let's generate some extra entropy in case you're on a fresh boot of a virtual machine, or your random number generator has been compromised.")
+        click.echo("Please bang on the keyboard for as long as you like.")
+        extra_entropy = click.prompt("There's no reason to record this value, it cannot be used to recover your keys")
+
+        if verbose:
+            click.echo(extra_entropy)
+
+        user_wallet_obj = Wallet.new_random_wallet(network=network,
+                user_entropy=extra_entropy)
         mpriv = user_wallet_obj.serialize_b58(private=True)
         mpub = user_wallet_obj.serialize_b58(private=False)
         click.echo('Your master PRIVATE key is: %s' % mpriv)
@@ -373,7 +631,7 @@ def cli(wallet, verbose):
 
 if __name__ == '__main__':
     '''
-    For invocation like this:
+    For (rare) invocation like this:
     python bmoney.py
     '''
     cli()
--- utils.py
+++ utils.py
@@ -5,10 +5,17 @@ from bitmerchant.wallet import Wallet
 
 from hashlib import sha256
 
-# collection of helper methods that will probably be moved to either the
-# bitmerchant or blockcypher libraries
+from blockcypher.utils import is_valid_coin_symbol, is_valid_hash
 
 
+# collection of helper methods
+# some may be moved to either the bitmerchant or blockcypher libraries
+
+# TODO: move to COIN_SYMBOL_ODICT_LIST once popping issue resolved in library
+COIN_SYMBOL_LIST = ['btc', 'btc-testnet', 'ltc', 'doge', ]  # 'bcy']
+
+
+# TODO: duplicated in blockcypher.constants?
 FIRST4_MKEY_MAPPINGS = {
         # first4: [bitmerchant_network, blockcypher, coin_symbol]
         'XPRV': [BitcoinMainNet, 'btc'],
@@ -18,8 +25,16 @@ FIRST4_MKEY_MAPPINGS = {
         'LTPV': [LitecoinMainNet, 'ltc'],
         'LTUP': [LitecoinMainNet, 'ltc'],
         'DGPV': [DogecoinMainNet, 'doge'],
-        'DGUP': [DogecoinMainNet, 'doge'],
+        'DGPV': [DogecoinMainNet, 'doge'],
+        # 'BPRV': [BlockCypherTestNet, 'bcy'],
+        # 'BPUB': [BlockCypherTestNet, 'bcy'],
         }
+
+COIN_SYMBOL_TO_BMERCHANT_NETWORK = {}
+for x in FIRST4_MKEY_MAPPINGS:
+    network = FIRST4_MKEY_MAPPINGS[x][0]
+    coin_symbol = FIRST4_MKEY_MAPPINGS[x][1]
+    COIN_SYMBOL_TO_BMERCHANT_NETWORK[coin_symbol] = network
 
 
 def guess_coin_from_mkey(mkey):
@@ -40,11 +55,109 @@ def guess_cs_from_mkey(mkey):
     return str(guess_coin_from_mkey(mkey)[1])
 
 
-def find_hexkeys_from_bip32masterkey(pub_address, master_key,
+def get_tx_url(tx_hash, coin_symbol):
+    assert is_valid_coin_symbol(coin_symbol), coin_symbol
+    assert is_valid_hash(tx_hash), tx_hash
+    return 'https://live.blockcypher.com/%s/tx/%s/' % (coin_symbol, tx_hash)
+
+
+def find_hexkeypair_from_bip32key_bc(pub_address, master_key,
         network=BitcoinMainNet, starting_pos=0, depth=100):
     '''
     Given a bip32 master (extended) key, traverse both internal and external
-    paths to `depth` looking for a matching key.
+    paths to `depth` looking for a matching key. In blockcypher parlance,
+    this is checking subchain_indexes == (0, 1)
+
+    This does not use the bip32 default wallet layout as that requires a
+    hardened child wallet, which necesitates the private key or complicated
+    management of multiple extended public keys.
+
+    `pub_address` looks like this for BTC: 1abc123...
+    `master_key` looks like this for BTC: xpriv123abc (or xpub123abc)
+
+    Returns a dict of:
+      - privkeyhex
+      - pubkeyhex
+      - path
+
+    Which can be used directly in TX signing.
+    If extended_key is a public key then method returns no privkeyhex
+    and cannot be used TX signing.
+    '''
+
+    # FIXME: deal with circular imports (depends on where this code ends up)
+    # defensive check:
+    # assert is_valid_address(pub_address), pub_address
+
+    wallet_obj = Wallet.deserialize(master_key, network=network)
+    external_chain_wallet = wallet_obj.get_child(0)  # m/0
+    internal_chain_wallet = wallet_obj.get_child(1)  # m/1
+
+    # traverse path
+    for x in range(starting_pos, starting_pos+depth+1):
+        external_path = "m/0/%d" % x
+        internal_path = "m/1/%d" % x
+        external_child = external_chain_wallet.get_child(x)
+        internal_child = internal_chain_wallet.get_child(x)
+        if external_child.to_address() == pub_address:
+            if external_child.private_key:
+                privkeyhex = external_child.private_key.get_key()
+            else:
+                privkeyhex = None
+            return {
+                    'privkeyhex': privkeyhex,
+                    'pubkeyhex': external_child.public_key.get_key(
+                        compressed=True),
+                    'path': external_path,
+                    }
+        if internal_child.to_address() == pub_address:
+            if internal_child.private_key:
+                privkeyhex = internal_child.private_key.get_key()
+            else:
+                privkeyhex = None
+            return {
+                    'privkeyhex': privkeyhex,
+                    'pubkeyhex': internal_child.public_key.get_key(
+                        compressed=True),
+                    'path': internal_path,
+                    }
+    # No matches
+    return {
+            'privkeyhex': None,
+            'pubkeyhex': None,
+            'path': None,
+            }
+
+    # traverse path
+    for x in range(starting_pos, starting_pos+depth+1):
+        path = "m/%d" % x
+        child = wallet_obj.get_child_for_path(path)
+        if child.to_address() == pub_address:
+            if wallet_obj.private_key:
+                privkeyhex = child.private_key.get_key()
+            else:
+                privkeyhex = None
+            return {
+                    'privkeyhex': privkeyhex,
+                    'pubkeyhex': child.public_key.get_key(compressed=True),
+                    'path': path,
+                    }
+    # No matches
+    return {
+            'privkeyhex': None,
+            'pubkeyhex': None,
+            'path': None,
+            }
+
+
+def find_hexkeypair_from_bip32key_linear(pub_address, master_key,
+        network=BitcoinMainNet, starting_pos=0, depth=100):
+    '''
+    Given a bip32 master (extended) key, traverse linearly to `depth` looking
+    for a matching key.
+
+    So it would go through m/0, m/1, m/2, m/3, etc.
+    This path is not used in bwallet.
 
     `pub_address` looks like this for BTC: 1abc123...
     `master_key` looks like this for BTC: xpriv123abc (or xpub123abc)
@@ -87,83 +200,24 @@ def find_hexkeys_from_bip32masterkey(pub_address, master_key,
             }
 
 
-def find_hexkeys_from_bip32masterkey_standardpath(pub_address, master_key,
-        network=BitcoinMainNet, starting_pos=0, depth=100):
-    '''
-    Given a bip32 master (extended) key, traverse both internal and external
-    paths to `depth` looking for a matching key.
-
-    `pub_address` looks like this for BTC: 1abc123...
-    `master_key` looks like this for BTC: xpriv123abc (or xpub123abc)
-
-    Returns a dict of:
-      - privkeyhex
-      - pubkeyhex
-      - path
-
-    Which can be used directly in TX signing.
-    If extended_key is a public key then method returns no privkeyhex
-    and cannot be used TX signing.
-    '''
-
-    # FIXME: deal with circular imports (depends on where this code ends up)
-    # defensive check:
-    # assert is_valid_address(pub_address), pub_address
-
-    wallet_obj = Wallet.deserialize(master_key, network=network)
-
-    # traverse internal and external path in fast order:
-    for x in range(starting_pos, starting_pos+depth+1):
-        int_path = "m/0/%d" % x
-        ext_path = "m/1/%d" % x
-        int_child = wallet_obj.get_child_for_path(int_path)
-        ext_child = wallet_obj.get_child_for_path(ext_path)
-        if int_child.to_address() == pub_address:
-            if wallet_obj.private_key:
-                privkeyhex = int_child.private_key.get_key()
-            else:
-                privkeyhex = None
-            return {
-                    'privkeyhex': privkeyhex,
-                    'pubkeyhex': int_child.public_key.get_key(compressed=True),
-                    'path': int_path,
-                    }
-        if ext_child.to_address() == pub_address:
-            if wallet_obj.private_key:
-                privkeyhex = ext_child.private_key.get_key()
-            else:
-                privkeyhex = None
-            return {
-                    'privkeyhex': privkeyhex,
-                    'pubkeyhex': ext_child.public_key.get_key(compressed=True),
-                    'path': ext_path,
-                    }
-    # No matches
-    return {
-            'privkeyhex': None,
-            'pubkeyhex': None,
-            'path': None,
-            }
-
-
-def find_path_from_bip32masterkey(pub_address, master_key,
+def find_path_from_bip32key_bc(pub_address, master_key,
         network=BitcoinMainNet, starting_pos=0, depth=100):
     '''
     Especially useful when using extended public keys for the master key
     '''
-    return find_hexkeys_from_bip32masterkey(pub_address=pub_address,
+    return find_hexkeypair_from_bip32key_bc(pub_address=pub_address,
             master_key=master_key, network=network, starting_pos=starting_pos,
             depth=depth)['path']
 
 
-def find_paths_from_bip32masterkey(pub_address_list, master_key,
+def find_paths_from_bip32key_bc(pub_address_list, master_key,
         network=BitcoinMainNet, starting_pos=0, depth=100):
     '''
     Bulk method for find_path_from_bip32masterkey
     '''
     paths = []
     for pub_address in pub_address_list:
-        path = find_path_from_bip32masterkey(
+        path = find_path_from_bip32key_bc(
                 pub_address=pub_address,
                 master_key=master_key,
                 network=network,
@@ -174,11 +228,15 @@ def find_paths_from_bip32masterkey(pub_address_list, master_key,
     return paths
 
 
-def get_blockcypher_walletname_from_mpub(mpub):
+def get_blockcypher_walletname_from_mpub(mpub, subchain_indexes=[]):
     '''
     Blockcypher limits wallet names to 25 chars.
 
-    Hash the master pubkey and take the first 25 chars.
+    Hash the master pubkey (with subchain indexes) and take the first 25 chars.
+
+    Hackey determinstic method for naming.
     '''
     assert type(mpub) in (str, unicode), mpub
+    if subchain_indexes:
+        mpub += ','.join([str(x) for x in subchain_indexes])
     return sha256(mpub).hexdigest()[:25]
