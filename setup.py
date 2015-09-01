2514e176d0da9137488f1c022e1c20aca633a314
Michael Flaxman <mflaxman@gmail.com>
2015-09-01 11:23:34 -0400

    bunch version for pypi

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
         name='bcwallet',
-        version='1.1.1',
+        version='1.1.2',
         description='Simple BIP32 HD cryptocurrecy command line wallet',
         author='Michael Flaxman',
         author_email='mflaxman+blockcypher@gmail.com',
