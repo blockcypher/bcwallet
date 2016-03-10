5a4b98275d605fdee56578bab22137b7fbace284
Michael Flaxman <mflaxman@gmail.com>
2016-03-10 17:33:04 -0500

    add version checker. upgrade to latest version of blockcypher library.

---
+++ bcwallet/version_checker.py
@@ -0,0 +1,14 @@
+import requests
+import re
+
+
+GITHUB_URL = 'https://github.com/blockcypher/bcwallet'
+VERSION_URL = 'https://raw.githubusercontent.com/blockcypher/bcwallet/master/setup.py'
+
+
+def get_latest_bcwallet_version():
+    r = requests.get(VERSION_URL)
+    assert r.status_code == 200, 'Could Not Connect to GitHub (status code %s)' % r.status_code
+    matches = re.findall("version='(.*?)\'", r.content)
+    assert matches, 'bcwallet version not found on github'
+    return matches[0]
